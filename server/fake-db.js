c3230fdebf51be143567933fca01c5deeb5f7c57
sathiyamoorthi <sathiyamoorthiips001@gmail.com>
2019-05-20 23:57:23 +0530

    add endpoints for create rentals and search rental by city

--- server/fake-db.js
+++ server/fake-db.js
@@ -1,69 +1,37 @@
 const Rental =require('./models/rental')
 const User =require('./models/user');
+const fakeDbData =require('./data.json');
+const Booking = require('./models/booking');
 class FakeDb{
     constructor(){
-        this.rentals = [{
-            title: "Nice view on ocean",
-            city: "San Francisco",
-            street: "Main street",
-            category: "condo",
-            image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-            bedrooms: 4,
-            shared: true,
-            description: "Very nice apartment in center of the city.",
-            dailyRate: 43
-            },
-            {
-            title: "Modern apartment in center",
-            city: "New York",
-            street: "Time Square",
-            category: "apartment",
-            image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-            bedrooms: 1,
-            shared: false,
-            description: "Very nice apartment in center of the city.",
-            dailyRate: 11
-            },
-            {
-            title: "Old house in nature",
-            city: "Spisska Nova Ves",
-            street: "Banicka 1",
-            category: "house",
-            image: "https://booksync-jerga-prod.s3.amazonaws.com/uploads/rental/image/5/image.jpeg",
-            bedrooms: 5,
-            shared: true,
-            description: "Very nice apartment in center of the city.",
-            dailyRate: 23
-}];
-    this.users =[{
-        usernmae :"Test User",
-        email :"test@gmail.com",
-        password:"testtest"
-    },
-    {
-        usernmae :"Test User2",
-        email :"test2@gmail.com",
-        password:"testtest"
-    }];
+        this.rentals= fakeDbData.rentals;
+        this.users= fakeDbData.users;
     }
-    async cleanDb() {
+    async cleanDb(){
         await User.remove({});
-       await Rental.remove({});
+        await Rental.remove({});
+        await Booking.remove({});
+
     }
-    pushDataToDb(){
-        const user =new User(this.users[0]);
-        const user2 =new User(this.users[1]);//above defined user this.users[0]
+    pushDataToDb() {
+        const user = new User(this.users[0]);
+        const user2 = new User(this.users[1]);
+    
         this.rentals.forEach((rental) => {
-            const newRental = Rental(rental);
-            newRental.user =user; //assign the user to rentals
-            user.rentals.push(newRental)//push rentals to user rental
-            newRental.save();//means rental was created by this user
+          const newRental = new Rental(rental);
+          newRental.user = user;
+    
+          user.rentals.push(newRental);
+          newRental.save();
         });
-      user.save();
-      user2.save();
-    }
-       async seedDb(){
-        await this.cleanDb()
+    
+        user.save();
+        user2.save();
+      }
+    
+    
+    async seedDb(){
+        await this.cleanDb();
         this.pushDataToDb();
     }
 }
--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,30 +1,62 @@
 const express =require('express');
 const router = express.Router();
 const Rental =require('../models/rental');
+const User = require('../models/user')
 const UserCtrl = require('../controllers/user')
+const {normalizeErrors} =require('../helpers/mongoose');
 router.get('/secret',UserCtrl.authMiddleware, function(req,res){
     res.json({"secret": true})
 })
-
-router.get('',function(req, res){
-   Rental.find({})
-         .select('-bookings')
-         .exec(function(err,foundRentals){
-       res.json(foundRentals);
-   })
-});
-
 router.get('/:id',function(req, res){
-    const rentalId = req.params.id;
-    Rental.findById(rentalId)
-          .populate('user','username -_id')
-          .populate('bookings','startAt endAt -_id')
-          .exec( function(err,foundRental)
-          {
-        if(err){
-          return  res.status(422).send({errors:[{title: 'Rental Error', detail :'could not find Rental'}]});
-        }
-        return res.json(foundRental);
-    });
+  const rentalId = req.params.id;
+  Rental.findById(rentalId)
+        .populate('user','username -_id')
+        .populate('bookings','startAt endAt -_id')
+        .exec( function(err,foundRental)
+        {
+      if(err){
+        return  res.status(422).send({errors:[{title: 'Rental Error', detail :'could not find Rental'}]});
+      }
+      return res.json(foundRental);
+  });
 });
+router.post('',UserCtrl.authMiddleware,function(req,res){
+  const {title,city,street,category,image,shared,bedrooms,description,dailyRate}= req.body;
+  const user =res.locals.user;
+  const rental = new Rental({title,city,street,category,image,shared,bedrooms,description,dailyRate});
+  rental.user =user
+  Rental.create(rental,function(err,newRental){
+    if(err){
+      return res.status(422).send({errors:normalizeErrors(err.errors)}); 
+    }
+    User.update({_id :user.id},{$push :{rentals:newRental}},function(){})
+    return res.json(newRental)
+  })
+})
+router.get('',function(req, res){
+  const city =req.query.city;
+  if(city){
+    Rental.find({city:city.toLowerCase()})
+    .select('-bookings')
+    .exec(function(err,filteredRentals){
+      if(err){
+        return res.status(422).send({errors:normalizeErrors(err.errors)});
+
+      }
+      if(filteredRentals.length===0){
+        return  res.status(422).send({errors:[{title: 'Rental Error', detail :`There are no rentals for city ${city}`}]});  
+      }
+      return res.json(filteredRentals);
+    })
+  }else{
+    Rental.find({})
+    .select('-bookings')
+    .exec(function(err,foundRentals){
+  res.json(foundRentals);
+})
+  }
+   
+});
+
+
 module.exports =router;
